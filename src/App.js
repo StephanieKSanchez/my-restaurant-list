a112463709884cfc4a442d476ca6f8b1afd98c44
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-23 11:29:00 -0400

    added basic navigation & routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import {  Layout } from 'antd'
 import RestaurantList from './components/RestaurantList';
@@ -9,15 +10,22 @@ const { Header, Content } = Layout;
 
 function App() {
   return (
+  <BrowserRouter>
     <Layout className='layout'>
       <Header>
         <Menubar />
       </Header>
       <Content>
-        <RestaurantList />
+        <Routes>
+          <Route path='/random' element={<h1>Random</h1>} />
+          <Route path='/add' element={<h1>Add Restaurant</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/' element={<RestaurantList />} />
+        </Routes>
       </Content>
         
       </Layout>
+    </BrowserRouter>
   );
 }
 
